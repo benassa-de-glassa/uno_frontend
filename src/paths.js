18a830b49c985b4d3314b014d3707a677136d036
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-03-24 13:37:15 +0100

    correct herokuapp path

--- src/paths.js
+++ src/paths.js
@@ -1,2 +1,3 @@
 // export const API_URL = process.env.REACT_APP_API_URL
-export const API_URL = 'https://unser-tolles-uno-backend.heroku.com/'
+export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
+export const WS_URL = 'ws://unser-tolles-uno-backend.herokuapp.com/'
