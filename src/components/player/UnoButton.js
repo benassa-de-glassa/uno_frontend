786447d6731e616d8e94acde60129d0e63efd539
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-03-29 21:16:53 +0200

    formatting

--- src/components/player/UnoButton.js
+++ src/components/player/UnoButton.js
@@ -4,21 +4,21 @@ import { Button } from 'react-bootstrap'
 import PlayerContext from '../../context/PlayerContext';
 
 function UnoButton() {
-    return (
-        <PlayerContext.Consumer>
-            { (value) => 
-            <div style={{float:"left"}}>
-            { !value.state.saidUno &&
+  return (
+    <PlayerContext.Consumer>
+      { (value) => 
+        <div style={{float:"left"}}>
+          { !value.state.saidUno &&
                 <Button 
-                    onClick = { value.sayUno }
+                  onClick = { value.sayUno }
                 >
                     Uno
                 </Button>
-            }
-            </div>
-            }
-        </PlayerContext.Consumer>
-    )
+          }
+        </div>
+      }
+    </PlayerContext.Consumer>
+  )
 }
 
 export default UnoButton
