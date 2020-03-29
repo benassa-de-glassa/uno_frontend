2d5c4b530f413d99d0b97cdaeabe3ee2cf400926
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-03-29 13:57:12 +0200

    update wss path for heroku

--- src/paths.js
+++ src/paths.js
@@ -1,3 +1,3 @@
 // export const API_URL = process.env.REACT_APP_API_URL
 export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
-export const WS_URL = 'ws://unser-tolles-uno-backend.herokuapp.com/'
+export const WS_URL = 'wss://unser-tolles-uno-backend.herokuapp.com/'
