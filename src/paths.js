a97d010b6df8b376f7a495054521a40813f20add
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-04-05 20:29:26 +0200

    path for heroku

--- src/paths.js
+++ src/paths.js
@@ -1,7 +1,7 @@
 // to run it from local API
-export const API_URL = "http://localhost:8000/"
-export const WS_URL = "ws://localhost:8000/"
+// export const API_URL = "http://localhost:8000/"
+// export const WS_URL = "ws://localhost:8000/"
 
 // To run it from heroku
-//export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
-//export const WS_URL = 'wss://unser-tolles-uno-backend.herokuapp.com/'
+export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
+export const WS_URL = 'wss://unser-tolles-uno-backend.herokuapp.com/'
