73da382b1b7eb53ea350c1ae3ea1a44360d6d541
Benedikt Schmidhuber <schmidhuber.benedikt@gmail.com>
2020-04-05 20:43:29 +0200

    Merge branch 'master' of github.com:benassa-de-glassa/uno_frontend

--- src/paths.js
+++ src/paths.js
@@ -1,3 +1,7 @@
-// export const API_URL = process.env.REACT_APP_API_URL
-export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
-export const WS_URL = 'wss://unser-tolles-uno-backend.herokuapp.com/'
+// to run it from local API
+export const API_URL = "http://localhost:8000/"
+export const WS_URL = "ws://localhost:8000/"
+
+// To run it from heroku
+//export const API_URL = 'https://unser-tolles-uno-backend.herokuapp.com/'
+//export const WS_URL = 'wss://unser-tolles-uno-backend.herokuapp.com/'
