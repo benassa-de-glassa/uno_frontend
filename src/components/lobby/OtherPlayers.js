1240e294d67bba42951737c61db48f2d308a817d
thilo <thiloschmid@bluewin.ch>
2020-04-10 15:13:16 +0200

    added indicators for finished players

--- src/components/lobby/OtherPlayers.js
+++ src/components/lobby/OtherPlayers.js
@@ -2,26 +2,44 @@ import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types';
 
 export class OtherPlayers extends Component {
-  //constructor(props){
-  //  super(props)
-  //}
-  
+
   componentDidUpdate(){
   }
 
-  render() {
+  render() { 
     let list = this.props.playerList.map( player => { 
+      let cardsIndicator,
+        backgroundColor;
+      if (player.finished) {
+        cardsIndicator = ""
+        backgroundColor = "lightskyblue"
+      } else if (player.saidUno) {
+        cardsIndicator = "[UNO]"
+        backgroundColor = "lightsalmon"
+      } else {
+        cardsIndicator = "[" + player.numberOfCards + "]"
+      }
+
+      let style = {"backgroundColor": backgroundColor, "border": "1px solid lightgrey"}
+
+      if (player.id === this.props.turn) {
+        style = {"backgroundColor" : "lightgreen", "border": "1px solid green"}
+      }
       return(
         <div 
           className="sidebar-player-list" 
           key={player.id} 
-          style={(player.id === this.props.turn) 
-            ? {'backgroundColor': 'lightgreen', 'border': '1px solid green'}
-            : {}}
+          style={style}
         >
           <p className="player-element">
-            <span><big>{player.king ? "\u265A" : "\u265F"}</big> {player.name} <strong>[{(player.saidUno) ? "UNO" : player.numberOfCards}]</strong></span>
-            {(this.props.king && !player.king) ? <span className="waste" onClick={() => this.props.kickPlayer(player.id)}>{"\u{1F5D1}"}</span> : ""}
+            <span>
+              <big>{player.king ? "\u265A" : "\u265F"}</big> 
+              {player.name} 
+              <strong>{cardsIndicator}</strong>
+            </span>
+            {(this.props.king && !player.king) 
+            ? <span className="waste" onClick={() => this.props.kickPlayer(player.id)}>{"\u{1F5D1}"}</span>
+            : ""}
           </p>
         </div>
       )
